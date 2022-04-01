8690a6e22232bddadc37a44afb4f638362d3ad52
indigobluw <lindquist20@gmail.com>
2022-04-01 19:14:58 +0200

    finished

--- src/main/java/WikipediaWeb.java
+++ src/main/java/WikipediaWeb.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -11,7 +12,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 public class WikipediaWeb {
     public static void main(String[] args) throws InterruptedException {
@@ -26,8 +27,6 @@ public class WikipediaWeb {
 
 
         System.out.println(driver.getTitle());
-
-        System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
@@ -35,16 +34,29 @@ public class WikipediaWeb {
 
         WebElement searchBox = driver.findElement(By.name("search"));
         WebElement searchButton = driver.findElement(By.name("go"));
+        WebElement wikipediaLogo;
+
         searchBox.sendKeys("Norge");
-
         System.out.println(searchBox.getAttribute("value"));
+        Thread.sleep(800);
+        searchBox.sendKeys(Keys.ARROW_DOWN);
+        Thread.sleep(500);
+        searchBox.sendKeys(Keys.RETURN);
 
-        searchBox.click();
+        //parser-output
+
+        String textOfPage = driver.findElement(By.className("mw-body")).getText();
+        System.out.println(textOfPage);
+
+        Thread.sleep(1000);
+        wikipediaLogo = driver.findElement(By.id("p-logo"));
+        wikipediaLogo.click();
 
         searchButton.getText();
         searchBox = driver.findElement(By.name("search"));
         searchBox.getAttribute("value");
 
+        Thread.sleep(10000);
 
         driver.quit();
     }
